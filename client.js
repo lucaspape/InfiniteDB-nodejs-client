6d205f6c6b350431e7ee4792a5f891a1b9170251
Lucas Pape <lucaspape2002@protonmail.com>
2022-05-29 19:28:34 +0200

    Implement basic client

---
+++ client.js
@@ -0,0 +1,41 @@
+const WebsocketClient = require('./websocket_client')
+
+module.exports = class Client {
+    constructor(host, database) {
+        this.host = host
+        this.database = database
+    }
+
+    static async getDatabases(host){
+        return await (await fetch(`http://${host.hostname}:${host.port}/databases`)).json()
+    }
+
+    static async createDatabase(host, name){
+        return await (await fetch(`http://${host.hostname}:${host.port}/database`, { method: "POST", body: JSON.stringify({ name: name })})).json()
+    }
+
+    async connect(){
+        this.client = new WebsocketClient(this.host, this.database)
+        await this.client.connect()
+    }
+
+    async getDatabase(){
+        return await this.client.getDatabase()
+    }
+
+    async getDatabaseTables(){
+        return await this.client.getDatabaseTables()
+    }
+
+    async createTableInDatabase(tableName, fields){
+        return await this.client.createTableInDatabase(tableName, fields)
+    }
+
+    async getFromDatabaseTable(tableName, request){
+        return await this.client.getFromDatabaseTable(tableName, request)
+    }
+
+    async insertToDatabaseTable(tableName, object){
+        return await this.client.insertToDatabaseTable(tableName, object)
+    }
+}
---
+++ websocket_client.js
@@ -0,0 +1,107 @@
+const WebSocketClient = require('websocket').client
+const {v4: uuidv4} = require('uuid')
+
+module.exports = class WebsocketClient {
+    constructor(host, database) {
+        this.host = host
+        this.database = database
+
+        this.connected = false
+
+        this.requests = new Map()
+    }
+
+    connect(){
+        return new Promise((resolve, reject) => {
+            this.ws = new WebSocketClient()
+
+            this.ws.on("connectFailed", (error) => {
+                reject(error)
+            })
+
+            this.ws.on("connect", (connection) => {
+                this.connection = connection
+
+                this.connection.on("error", (error) => {
+                    console.log('InfiniteDB websocket connection closed with error ' + error)
+                    this.connected = false
+                })
+
+                this.connection.on("close", () => {
+                    console.log('InfiniteDB websocket connection closed')
+                    this.connected = false
+                })
+
+                this.connection.on("message", (msg) => {
+                    const res = JSON.parse(msg.utf8Data)
+
+                    switch(res.message) {
+                        case "HELO":
+                            this.connected = true
+                            resolve()
+                            break
+                        default:
+                            let requestId = res.requestId
+
+                            if(requestId){
+                                let request = this.requests.get(requestId)
+
+                                if(res.status === 200){
+                                    request.resolve(res)
+                                }else{
+                                    request.reject(res)
+                                }
+                            }else{
+                                reject("Server response did not have a requestId")
+                            }
+
+                            break
+                    }
+                })
+            })
+
+            this.ws.connect(`ws://${this.host.hostname}:${this.host.port}/ws`)
+        })
+    }
+
+    #send(data){
+        if(this.connected){
+            this.connection.sendUTF(JSON.stringify(data))
+        }else{
+            throw "Not connected to websocket"
+        }
+    }
+
+    #sendRequest(request){
+        let requestId = uuidv4()
+
+        return new Promise(((resolve, reject) => {
+            this.requests.set(requestId, {resolve: resolve, reject: reject})
+
+            request.requestId = requestId
+            request.name = this.database
+
+            this.#send(request)
+        }))
+    }
+
+    async getDatabase(){
+        return await this.#sendRequest({ method: "getDatabase" })
+    }
+
+    async getDatabaseTables(){
+        return await this.#sendRequest({ method: "getDatabaseTables" })
+    }
+
+    async createTableInDatabase(tableName, fields){
+        return await this.#sendRequest({ method: "createTableInDatabase", tableName: tableName, fields: fields })
+    }
+
+    async getFromDatabaseTable(tableName, request){
+        return await this.#sendRequest({ method: "getFromDatabaseTable", tableName: tableName, request: request })
+    }
+
+    async insertToDatabaseTable(tableName, object){
+        return await this.#sendRequest({ method: "insertToDatabaseTable", tableName: tableName, object: object })
+    }
+}
